a9ed97fb168cafe908ace370e10f31a09f65982a
Lalan Chaudhary <chaudharylalan28@gmail.com>
2025-06-04 22:54:02 +0530

    rtregr

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { NavbarDemo } from './components/Navbar'
 import './App.css'
-import logo from './assets/Egglesscake.png'
 import HeroSection from './components/HeroSection'
 import AllCakes from './pages/AllCakes'
 import CakeDetails from './pages/CakeDetails'
 import Cart from './pages/Cart'
 import { CartProvider } from './context/CartContext'
 import Footer from './components/Footer'
-import Home from './components/Home'
 import Products from './components/Products'
 import Profile from './pages/Profile'
 import UserProfile from './pages/UserProfile'
@@ -18,31 +16,34 @@ import ContactUs from './pages/ContactUs'
 import Checkout from './pages/Checkout'
 import OrderSuccess from './pages/OrderSuccess'
 import { UserProvider } from './context/UserContext'
+import ScrollToTop from './ScrollToTop'
+
 const App = () => {
   return (
     <CartProvider>
       <UserProvider>
-      <Router>
-        <div className='bg-[#f4eee1] min-h-screen flex flex-col'>
-          <NavbarDemo />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HeroSection />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/all-cakes" element={<AllCakes />} />
-              <Route path="/cake/:id" element={<CakeDetails />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/user-profile" element={<UserProfile />} />
-              <Route path="/about-us" element={<AboutUs />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/order-success" element={<OrderSuccess />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+        <Router>
+          <ScrollToTop /> {/* ✅ Now placed correctly */}
+          <div className='bg-[#f4eee1] min-h-screen flex flex-col'>
+            <NavbarDemo />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<HeroSection />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/all-cakes" element={<AllCakes />} />
+                <Route path="/cake/:id" element={<CakeDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/user-profile" element={<UserProfile />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/contact-us" element={<ContactUs />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/order-success" element={<OrderSuccess />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
       </UserProvider>
     </CartProvider>
   )
